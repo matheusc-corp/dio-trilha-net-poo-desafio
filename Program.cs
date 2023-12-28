7326349a9c812d6899883491809f112198e8668c
Matheus Carvalho <matheuso.c08@gmail.com>
2023-12-28 15:46:52 -0300

    Incluído as propriedades pendentes e ajuste no construtor dna classe Smartphone. Feita sobrecarga do método InstalarAplicativo e realizados testes na classe Program

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone s1 = new Nokia("11943211234", "X2-01", "351122334455", 256);
+Smartphone s2 = new Iphone("11987655678", "Iphone 11 Pro", "355544332211", 512);
+
+Console.Write($"Aparelho {s2.Numero}: ");
+s2.Ligar();
+Console.Write($"Aparelho {s1.Numero}: ");
+s1.ReceberLigacao();
+
+s1.InstalarAplicativo("Whats App");
+s2.InstalarAplicativo("Clash of Clans");
